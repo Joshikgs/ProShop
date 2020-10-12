804bf79d5bfdeebd9def71af08b7d0696eb6160b
Joshua Nho <joshnho@gmail.com>
2020-10-11 21:30:21 -0400

    fixed typos

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,7 +21,7 @@ export const listProducts = () => async(dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            payload: error.response.data.message ? error.response.data.message : error.message
         })
     }
 }
@@ -39,7 +39,7 @@ export const listProductDetails = (productId) => async(dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            payload: error.response.data.message ? error.response.data.message : error.message
         })
     }
 }
