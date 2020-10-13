ec402bbd1b6e4e5e3b395b3bb241744668ee3f7e
Joshua Nho <joshnho@gmail.com>
2020-10-13 18:26:06 -0400

    created authMiddleware for user authorization to protected routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { loginUser } from '../controllers/userController.js'
+import { loginUser, getUserProfile } from '../controllers/userController.js'
+import { protectRoute } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/login', loginUser)
+router.route('/profile').get(protectRoute, getUserProfile)
 
 export default router
