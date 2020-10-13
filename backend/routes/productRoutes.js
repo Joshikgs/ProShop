e7a03a94eb74534b07ed8a0e40fb57a24ba5e7b4
Joshua Nho <joshnho@gmail.com>
2020-10-13 17:47:34 -0400

    created backend routes and controller for User model to login/authenticate users

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,14 +2,8 @@ import express from 'express'
 import { getProducts, getProductById } from '../controllers/productController.js'
 const router = express.Router()
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public 
-router.route('/').get(getProducts)
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
+router.route('/').get(getProducts)
 router.route('/:id').get(getProductById)
 
 export default router
