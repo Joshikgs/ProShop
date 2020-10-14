5af5601e2018e3612e7561e22f846173d15b6592
Joshua Nho <joshnho@gmail.com>
2020-10-14 15:48:35 -0400

    created actions and reducers to update profile; created profile screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
 
 const ProfileScreen = ({ location, history }) => {
     const [name, setName] = useState('')
@@ -18,7 +18,10 @@ const ProfileScreen = ({ location, history }) => {
     const { loading, error, user } = userDetails
 
     const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin    
+    const { userInfo } = userLogin
+
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
 
     useEffect(() => {
         if(!userInfo){
@@ -38,7 +41,12 @@ const ProfileScreen = ({ location, history }) => {
         if(password !== confirmPassword){
             setMessage('Passwords do not match')
         } else {
-            // DISPATCH UPDATE PROFILE
+            dispatch(updateUserProfile({
+                id: user._id,
+                name,
+                email,
+                password
+            }))
         }
     }
 
@@ -48,6 +56,7 @@ const ProfileScreen = ({ location, history }) => {
                 <h2>User Profile</h2>
                 {message && <Message variant='danger'>{message}</Message>}
                 {error && <Message variant='danger'>{error}</Message>}
+                {success && <Message variant='success'>Profile Updated</Message>}
                 {loading && <Loader />}
                 <Form onSubmit={submitHandler}>
                     <Form.Group controlId='name'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { userDetailsReducer, userLoginReducer, userRegisterReducer } from './reducers/userReducers'
-import { cartReducer} from './reducers/cartReducers'
+import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -11,7 +11,8 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
-    userDetails: userDetailsReducer
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
