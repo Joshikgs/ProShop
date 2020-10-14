59439707fd5118b768bb8b5daf6631d166a63811
Joshua Nho <joshnho@gmail.com>
2020-10-14 01:26:21 -0400

    created routes and controllers for updating user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { loginUser, registerUser, getUserProfile } from '../controllers/userController.js'
+import { loginUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import { protectRoute } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.route('/').post(registerUser)
 router.post('/login', loginUser)
-router.route('/profile').get(protectRoute, getUserProfile)
+router.route('/profile').get(protectRoute, getUserProfile).put(protectRoute, updateUserProfile)
 
 export default router
