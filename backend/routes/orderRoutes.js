01e3c3016e5bea4642705213675d704d3e9559bd
Joshua Nho <joshnho@gmail.com>
2020-10-14 22:48:45 -0400

    created backend route and controller for orders

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { addOrderItems } from '../controllers/orderController.js'
+import { protectRoute } from '../middleware/authMiddleware.js'
+const router = express.Router()
+
+router.route('/').post(protectRoute, addOrderItems)
+
+export default router
