586686d0b63abfd94e24e6f28396ddd9b1c8de8f
Joshua Nho <joshnho@gmail.com>
2020-10-14 20:32:47 -0400

    added indicator on top of each page in ordering process to show current step and navigate to previous steps

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import OrderSteps from '../components/OrderSteps'
 import { saveShippingAddress} from '../actions/cartActions'
 
-const ShippingScreen = ({ history }) => {
+const CheckoutScreen = ({ history }) => {
     const cart = useSelector(state => state.cart)
     const { shippingAddress } = cart
 
@@ -23,7 +24,8 @@ const ShippingScreen = ({ history }) => {
 
     return (
         <FormContainer>
-            <h1>Shipping</h1>
+            <OrderSteps step1 step2 />
+            <h1>Shipping Address</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
                         <Form.Label>Address</Form.Label>
@@ -50,4 +52,4 @@ const ShippingScreen = ({ history }) => {
     )
 }
 
-export default ShippingScreen
+export default CheckoutScreen
