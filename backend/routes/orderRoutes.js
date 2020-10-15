e5616dc4034dda7a7628647bf20ec3b9ec1e0544
Joshua Nho <joshnho@gmail.com>
2020-10-14 23:25:02 -0400

    created backend controller and route to get an order by ID

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protectRoute } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.route('/').post(protectRoute, addOrderItems)
+router.route('/:id').get(protectRoute, getOrderById)
 
 export default router
