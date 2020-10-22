c9185d54ea93b468635b119e15a908eb7354c40a
Joshua Nho <joshnho@gmail.com>
2020-10-22 05:17:53 -0400

    created backend endpoints for getUserById and updateUser for admins

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,22 @@
 import express from 'express'
-import { loginUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js'
+import { loginUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protectRoute, authAdmin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').post(registerUser).get(protectRoute, authAdmin, getUsers)
-router.post('/login', loginUser)
-router.route('/profile').get(protectRoute, getUserProfile).put(protectRoute, updateUserProfile)
-router.route('/:id').delete(protectRoute, authAdmin, deleteUser)
+router
+    .route('/')
+    .post(registerUser)
+    .get(protectRoute, authAdmin, getUsers)
+router
+    .post('/login', loginUser)
+router
+    .route('/profile')
+    .get(protectRoute, getUserProfile)
+    .put(protectRoute, updateUserProfile)
+router
+    .route('/:id')
+    .delete(protectRoute, authAdmin, deleteUser)
+    .get(protectRoute, authAdmin, getUserById)
+    .put(protectRoute, authAdmin, updateUser)
 
 export default router
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { logout } from '../actions/userActions'
-
 import { useHistory } from "react-router-dom"
 
 const Header = () => {
